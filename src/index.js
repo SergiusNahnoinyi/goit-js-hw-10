76cd7d43525c5ffe4df160f06635e0624c07476e
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2021-12-04 14:24:26 +0200

    Реализует функцию fetchCountries(name), которая делает HTTP-запрос на ресурс

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+fetchCountries();
+
+function fetchCountries() {
+  fetch('https://restcountries.com/v3.1/name/all')
+    .then(response => {
+      return response.json();
+    })
+    .then(country => {
+      console.log(country);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
