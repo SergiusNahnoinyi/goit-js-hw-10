ed225ccfb234854f7b7ba26ec4487dd402f386b9
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2021-12-05 12:04:55 +0200

    Реализует поиск страны по событию input
    Кавычки играют роль)

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,5 @@
 export function fetchCountries(name) {
-  fetch('https://restcountries.com/v3.1/name/all?fields=name,population,flags,languages')
+  fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,population,flags,languages`)
     .then(response => {
       return response.json();
     })
--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+const searchInput = document.querySelector('#search-box');
+searchInput.addEventListener('input', onSearch);
 
-fetchCountries();
+function onSearch(event) {
+  const searchQuery = event.currentTarget.value;
+  console.log(searchQuery);
+  fetchCountries(searchQuery);
+}
